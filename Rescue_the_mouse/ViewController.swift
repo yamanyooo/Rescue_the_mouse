84b342cb35d7f20e1eaa21358495ee43f2939440
yamanyooo <yohei@yohei-no-macbook-air.local>
2018-08-14 12:51:01 +0900

    Initial Commit

---
+++ Rescue_the_mouse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rescue_the_mouse
+//
+//  Created by yohei on 2018/08/14.
+//  Copyright © 2018年 yamanyon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
